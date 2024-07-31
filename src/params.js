cdea166ff44b26449428ee102082b2c78713b9aa
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-07-31 11:39:14 +0530

    Update params.js

--- src/params.js
+++ src/params.js
@@ -1,21 +1,23 @@
 const DEFAULT_QUALITY = 40;
 
 function params(req, res, next) {
-  const { url, jpeg, bw, l } = req.query;
+    const { url, jpeg, bw, l } = req.query;
 
-  if (!url) {
-    return res.end('bandwidth-hero-proxy');
-  }
+    if (!url) {
+        return res.end('bandwidth-hero-proxy'); // or send an appropriate response if URL is missing
+    }
 
-  const urls = Array.isArray(url) ? url.join('&url=') : url;
-  const cleanedUrl = urls.replace(/http:\/\/1\.1\.\d\.\d\/bmi\/(https?:\/\/)?/i, 'http://');
+    // Clean up and format the URL
+    const urls = Array.isArray(url) ? url.join('&url=') : url;
+    const cleanedUrl = urls.replace(/http:\/\/1\.1\.\d\.\d\/bmi\/(https?:\/\/)?/i, 'http://');
 
-  req.params.url = cleanedUrl;
-  req.params.webp = !jpeg;
-  req.params.grayscale = bw !== '0';
-  req.params.quality = parseInt(l, 10) || DEFAULT_QUALITY;
+    // Set parameters for image processing
+    req.params.url = cleanedUrl;
+    req.query.webp = !jpeg;
+    req.query.grayscale = bw !== '0';
+    req.query.quality = parseInt(l, 10) || DEFAULT_QUALITY;
 
-  next();
+    next();
 }
 
 module.exports = params;
