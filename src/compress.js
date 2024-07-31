d3ad4c8c1584dd9804413df639a2a1be0d85a9e2
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-07-31 12:54:34 +0530

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -1,23 +1,6 @@
 const sharp = require('sharp');
-
-function redirect(req, res) {
-    if (res.headersSent) {
-        return;
-    }
-    res.setHeader('content-length', 0);
-    res.removeHeader('cache-control');
-    res.removeHeader('expires');
-    res.removeHeader('date');
-    res.removeHeader('etag');
-    res.setHeader('location', encodeURI(req.params.url));
-    res.status(302).end();
-}
-
-function bypass(req, res, buffer) {
-    res.setHeader('x-proxy-bypass', 1);
-    res.setHeader('content-length', buffer.length);
-    res.status(200).send(buffer);
-}
+const redirect = require('./redirect');
+const bypass = require('./bypass');
 
 function compress(req, res, input) {
     const format = req.params.webp ? 'webp' : 'jpeg';
